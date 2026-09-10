33c830e2a5cbe4aaaaaed750d62ff4d297b113bf
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-09-10 21:17:44 +0800

    test(manifest): 纳入SSOT合同回归

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -72,6 +72,7 @@ done
 
 TESTS=(
   test_validate.sh
+  test_manifest_contract.sh
   test_python_launcher.sh
   test_asset_content_quality.sh
   test_format.sh
@@ -143,6 +144,7 @@ TESTS=(
 
 QUICK_TESTS=(
   test_validate.sh
+  test_manifest_contract.sh
   test_python_launcher.sh
   test_runtime_boundary.sh
   test_target_contracts.sh
