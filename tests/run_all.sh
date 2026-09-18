d175eef18459741b559d103b1b1351455fe8e1a2
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-09-18 18:47:41 +0800

    test: stop scheduling retired manifest split gate

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -78,7 +78,7 @@ TESTS=(
   test_manifest_consumer_boundary.sh
   test_manifest_composition_contract.sh
   test_manifest_composition_cli.sh
-  test_manifest_split_readiness.sh
+
   test_python_launcher.sh
   test_asset_content_quality.sh
   test_format.sh
@@ -161,7 +161,7 @@ QUICK_TESTS=(
   test_manifest_consumer_boundary.sh
   test_manifest_composition_contract.sh
   test_manifest_composition_cli.sh
-  test_manifest_split_readiness.sh
+
   test_python_launcher.sh
   test_runtime_boundary.sh
   test_target_contracts.sh
