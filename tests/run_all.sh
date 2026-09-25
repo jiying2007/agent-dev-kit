b260dbadb17dfeffc6ba304c335898a4959b5b52
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-09-25 13:26:36 +0800

    test(runtime): 纳入 native trust 快速与完整回归

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -92,7 +92,9 @@ TESTS=(
   test_convert.sh
   test_runtime_boundary.sh
   test_target_contracts.sh
+  test_native_trust.sh
   test_target_adapter_spi.sh
+  test_native_trust.sh
   test_target_source_probe.sh
   test_effect_eval.sh
   test_product_maturity.sh
